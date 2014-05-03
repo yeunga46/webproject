bfe2a1c9ec2089e13de5765ce1d95ff970b7ae55
yeunga46 <yeunga46@students.rowan.edu>
2014-05-03 17:41:37 -0400

    Update quit.php

--- PHP/quit.php
+++ PHP/quit.php
@@ -1,10 +1,9 @@
 <?php
-
 require_once ('/export/home/fidukj40/source_html/web/webProject/Connect.php');
 $dbh = ConnectDB();
 
-$query = "INSERT into high_score(name, total_time, num_correct,total_questions)\
-values(:name,:time, :correct,:total);
+$query = 'INSERT into high_score(name, total_time, num_correct,total_questions) '.
+'VALUES(:name,:time, :correct,:total)';
 $stmt = $dbh->prepare($query);
 $name= $_GET['name'];
 $time= $_GET['time'];
@@ -16,7 +15,7 @@ $stmt->bindParam(':time',$time);
 $stmt->bindParam(':correct',$correct);
 $stmt->bindParam(':total',$total);
 
-//check here
-stmt->execute();
+$stmt->execute();
+
 
 ?>
