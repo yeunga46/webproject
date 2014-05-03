bd5a5a1ebd847fac782ba9886bc2a4bed422b664
Fidukj40 <jfiduk@comcast.net>
2014-05-03 17:19:09 -0400

    Create quit.php

---
+++ PHP/quit.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once ('/export/home/fidukj40/source_html/web/webProject/Connect.php');
+$dbh = ConnectDB();
+
+$query = "INSERT into high_score(name, total_time, num_correct,total_questions)\
+values(:name,:time, :correct,:total);
+$stmt = $dbh->prepare($query);
+$name= $_GET['name'];
+$time= $_GET['time'];
+$correct= $_GET['correct'];
+$total= $_GET['total'];
+
+$stmt->bindParam(':name',$name);
+$stmt->bindParam(':time',$time);
+$stmt->bindParam(':correct',$correct);
+$stmt->bindParam(':total',$total);
+
+//check here
+stmt->execute();
+
+?>
