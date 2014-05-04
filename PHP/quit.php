6a2bdc38165a4593131e16296ce8342e9c20cf53
Fidukj40 <jfiduk@comcast.net>
2014-05-04 15:01:22 -0400

    documentation

--- PHP/quit.php
+++ PHP/quit.php
@@ -1,11 +1,15 @@
 <?php
+//Stores a persons attempt in the highscore file.
+//Authors Jason Fiduk and Andy Yeung
+//Links to the Database
 require_once ('/export/home/fidukj40/source_html/web/webProject/Connect.php');
 $dbh = ConnectDB();
 $name= $_GET['name'];
 $time= $_GET['time'];
 $correct= $_GET['correct'];
 $total= $_GET['total'];
-
+//Checks to see if the name is 3 characters, the time is greater than zero,
+//and at least 9 questions have been attempted.
 if(strlen($name)==3 && $time >0 && $correct != null && $total >=9)
 {
 try{
@@ -17,7 +21,7 @@ $stmt->bindParam(':name',$name);
 $stmt->bindParam(':time',$time);
 $stmt->bindParam(':correct',$correct);
 $stmt->bindParam(':total',$total);
-
+//Executes the query
 $stmt->execute();
 }
 catch(PDOException $e)
